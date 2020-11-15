97661a33b692beddf0f484418768e17110a7ff64
Yoshiyuki Makino <yoshiyuki.makino@G-PC-00579205.local>
2020-11-15 22:41:28 +0900

    ‘fixed’

--- Gemfile
+++ Gemfile
@@ -57,3 +57,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'haml-rails'
 gem 'dotenv-rails'
+gem 'omniauth-twitter'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'application#root'
+  get '/auth/:provider/callback', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
